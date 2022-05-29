932bf9fc1d37b668dbb99869b397438720aadaee
Waseemalame <waseemalame45@gmail.com>
2022-05-28 17:39:03 -0700

    postbrowser component created

---
+++ frontend/src/components/PostBrowser/index.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts } from '../../store/posts';
+import CreatePostForm from '../CreatePostForm/index';
+import { NavLink, Route, useParams } from 'react-router-dom';
+
+import PostList from '../PostList';
+
+
+const PostBrowser = () => {
+
+  const dispatch = useDispatch();
+
+  // const posts = useSelector(state => {
+  //   console.log(state.posts, 'typeof state.posts')
+  // });
+  const posts = useSelector(state => {
+    return state.posts.list.map(postId => postId)
+  });
+  console.log(posts, 'POSTPOSTSPOTPOST')
+
+  console.log('hidfhsi')
+  useEffect(() => {
+
+    dispatch(getPosts())
+
+  }, [dispatch]);
+
+  return(
+    <div className='main'>
+      {/* <ul>
+        <CreatePostForm />
+      </ul> */}
+      <Route path="/">
+          <PostList />
+      </Route>
+    </div>
+  )
+}
+export default PostBrowser;
