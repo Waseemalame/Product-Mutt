73da13c107ae9ace78e567bd1152b5dbc0247e8a
Waseemalame <waseemalame45@gmail.com>
2022-05-29 20:40:14 -0700

    backend post route created

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const {requireAuth} = require('../../utils/auth')
 
 
 const { Post } = require('../../db/models');
@@ -16,18 +17,25 @@ router.get('/', asyncHandler(async function(_req, res) {
 
 }));
 
-router.post('/', asyncHandler(async function (req, res) {
-    console.log(req.body);
-    const post = await Post.create(req.body);
-    console.log(post);
-    return res.json(post);
+router.post('/', requireAuth, asyncHandler(async function (req, res) {
 
-  })
+  console.log('BACKEND POST')
+    console.log(req.body, 'req.body');
+    const { title, content, media, userId } = req.body
+    const post = await Post.create({
+      title,
+      content,
+      media,
+      userId
+    });
+
+    return res.json(post);
+    })
   );
 
   module.exports = router;
 
-  // const id = await PokemonRepository.create(req.body);
+  // const id = await PokemonRepository.create(req.body);S
   // console.log('BACKEND POKEMON POST AFTER ADD TO DB - id -> ', id);
   // return res.redirect(`${req.baseUrl}/${id}`);
   // const id = await PokemonRepository.create(req.body);
