62f09fdc39863f3153204a44bc6b4cac91826f78
Waseemalame <waseemalame45@gmail.com>
2022-06-02 08:11:47 -0700

    useSelector edit and background css

--- frontend/src/components/PostDetails/index.js
+++ frontend/src/components/PostDetails/index.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, useParams } from 'react-router-dom'
 import { getPostDetails } from '../../store/posts';
+import PostsComments from '../PostsComments';
+import { getComments } from '../../store/posts';
 import EditPostForm from '../EditPostForm';
 import Fab from '../Fab';
 import './PostDetails.css'
@@ -14,11 +16,24 @@ const PostDetails = () => {
   const id = postId.id
 
   // const [showForm, setShowForm] = useState(false);
-  const post = useSelector(state => Object.values(state.posts)[id - 1])
-  // console.log(post, 'POSTPOSTPOST')
+  const post = useSelector(state => {
+    console.log(state.posts[id], 'state.posts from postDetails')
+    // return Object.values(state.posts)[id - 1]
+    return state.posts[id]
+
+  })
+
+  // const post = useSelector(state => state.post[id]);
+
+  console.log(post, 'POSTPOSTPOST')
+
   useEffect(() => {
     dispatch(getPostDetails(id))
   }, [dispatch, id])
+  useEffect(() => {
+    dispatch(getComments(id))
+  }, [dispatch, id])
+
 
   return (
     <>
@@ -35,9 +50,11 @@ const PostDetails = () => {
             <li>
               <p>{post.content}</p>
             </li>
+        {/* <Comments /> */}
 
           </ul>
         <EditPostForm />
+        <PostsComments post={post} />
         </div>
 
     </>
