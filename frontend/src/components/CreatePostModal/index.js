628f2a40a3b6e3ef166b916ae3933ae3afb1e229
Waseemalame <waseemalame45@gmail.com>
2022-06-02 19:50:50 -0700

    create post modal complete

---
+++ frontend/src/components/CreatePostModal/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import CreatePostForm from '../CreatePostForm';
+import './CreatePostModal.css'
+function CreatePostModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>Create Post</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <CreatePostForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default CreatePostModal;
