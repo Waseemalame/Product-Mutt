b47ba232a0e4e98a3543abb7db99e712457aa93e
Waseemalame <waseemalame45@gmail.com>
2022-06-03 15:32:46 -0700

    backend bug fixed

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -2,51 +2,31 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const {requireAuth} = require('../../utils/auth')
 const postRepository = require('../../db/post-repository')
-
-
 const { Post, Comment, User } = require('../../db/models');
-
 const router = express.Router();
-
   /* GET ALL POSTS */
 router.get('/', asyncHandler(async function(_req, res) {
-
-
   const posts = await Post.findAll()
   return res.json(posts);
-
 }));
-
   /* GET ONE POST */
   router.get('/:id', asyncHandler(async function(req, res) {
-
     const post = await postRepository.one(req.params.id)
-
     return res.json(post);
-
   }));
-
-
 /* GET Comments */
 router.get('/:id/comments', asyncHandler(async function(req, res) {
-
   const post = await postRepository.one(req.params.id)
   const postId = post.id
-
   const comments = await Comment.findAll({
     include: User,
     where: {
       postId
     }
   });
-
-
   return res.json(comments);
 }));
-
 router.post('/', requireAuth, asyncHandler(async function (req, res) {
-
-
     const { title, content, media, userId } = req.body
     const post = await Post.create({
       title,
@@ -54,37 +34,25 @@ router.post('/', requireAuth, asyncHandler(async function (req, res) {
       media,
       userId
     });
-
     return res.json(post);
     })
   );
 router.put('/:id', requireAuth, asyncHandler(async function (req, res) {
-
     const userId = req.user.id;
     req.body.userId = userId;
-
-
     // const id = await postRepository.update(req.body);
     // const post = await postRepository.one(req.params.id);
     const post = await Post.findByPk(req.params.id)
     const { title, content, media } = req.body
-
     post.update({
       title,
       content,
       media
     })
-
     return res.json(post);
     })
   );
-
-
-
-
   router.delete('/:id', requireAuth, asyncHandler(async function (req, res) {
-
-
     const {id} = req.params;
     const post = await Post.findByPk(id);
     await post.destroy();
@@ -93,7 +61,6 @@ router.put('/:id', requireAuth, asyncHandler(async function (req, res) {
   );
     /* CREATE COMMENT  */
     router.post('/:id/comments/', requireAuth, asyncHandler(async function (req, res) {
-
       const {
         content: commentContent,
         userId,
@@ -104,7 +71,6 @@ router.put('/:id', requireAuth, asyncHandler(async function (req, res) {
         content: commentContent,
         userId,
         postId
-
       })
       const newComment = await Comment.findByPk(comment.id, {include: User})
     return res.json(newComment);
@@ -126,19 +92,13 @@ router.put('/:id', requireAuth, asyncHandler(async function (req, res) {
     //   "/:postId/comments/:id",
     //   requireAuth,
     //   asyncHandler(async function (req, res) {
-
     //   const post = await Post.findByPk(req.params.postId)
-
     //   post.update({
     //     title,
     //     content,
     //     media
     //   })
-
     //   return res.json(post);
     //   })
     // );
-
-
-
   module.exports = router;
