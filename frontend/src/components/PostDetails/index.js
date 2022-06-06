555cb481ffe4a8b27699f62c6aa1e620e9ef1050
Waseemalame <waseemalame45@gmail.com>
2022-06-05 22:26:34 -0700

    removed unused files added styles

--- frontend/src/components/PostDetails/index.js
+++ frontend/src/components/PostDetails/index.js
@@ -1,11 +1,8 @@
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, useHistory, useParams } from 'react-router-dom'
-import { getPostDetails } from '../../store/posts';
+import { useHistory} from 'react-router-dom'
 import PostsComments from '../PostsComments';
 import { getComments } from '../../store/posts';
-import EditPostForm from '../EditPostForm';
-import Fab from '../Fab';
 import EditPostModal from '../EditPostModal';
 import { removePost } from '../../store/posts';
 
@@ -13,46 +10,20 @@ import './PostDetails.css'
 
 const PostDetails = ({ post, setShowModal }) => {
 
-  const param = useParams();
-  // console.log(param.id, 'postId') // {} ??
-
   const dispatch = useDispatch();
-  // const id = postId.id
 
-  // const [showForm, setShowForm] = useState(false);
-  // const onePost = useSelector(state => {
-    // console.log(state.posts[id], 'state.posts from postDetails')
-    // return Object.values(state.posts)[id - 1]
-
-
-    // return state.posts[post.id]
-
-  // })
   const userId = useSelector(state => {
     if(state.session.user){
       return state.session.user.id
     }
   })
 
-  // const post = useSelector(state => state.posts[param.id]);
-
 
   const history = useHistory();
-
-  // useEffect(() => {
-  //   dispatch(getPostDetails(post.id))
-
-  // }, [dispatch, post.id])
-  // useEffect(() => {
-  //   dispatch(removePost(id))
-  // }, [dispatch, id])
-
-
 
   return (
     <>
       <div className='details-container'>
-      {/* <button>Close</button> */}
 
           <ul className='details-list'>
             <li>
@@ -64,14 +35,9 @@ const PostDetails = ({ post, setShowModal }) => {
             <li>
               <p>{post.content}</p>
             </li>
-        {/* <Comments /> */}
+
 
           </ul>
-          {/* {userId === post.userId &&
-            <div>
-            <button>delete</button>
-            </div>
-            } */}
           {userId === post.userId &&
             <div>
             <EditPostModal post={post} />
