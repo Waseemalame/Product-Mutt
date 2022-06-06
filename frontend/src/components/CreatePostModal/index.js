55d9bb9a1d12b75a7ed9e72f1ac1c59c4f9d087b
Waseemalame <waseemalame45@gmail.com>
2022-06-06 00:42:53 -0700

    extra styles added

--- frontend/src/components/CreatePostModal/index.js
+++ frontend/src/components/CreatePostModal/index.js
@@ -10,7 +10,7 @@ function CreatePostModal() {
       <div className="create-form-modal-container">
         <button className='create-post-btn' onClick={() => setShowModal(true)}>Create Post</button>
         {showModal && (
-          <Modal onClose={() => setShowModal(false)}>
+          <Modal className="create-post-modal-container" onClose={() => setShowModal(false)}>
             <CreatePostForm setShowModal={setShowModal} />
           </Modal>
         )}
