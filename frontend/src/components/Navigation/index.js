2dd8a250720ef8195dffc7fc6a92ed83b03e19c7
Waseemalame <waseemalame45@gmail.com>
2022-06-05 22:24:37 -0700

    added demo-login user

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import CreatePostModal from '../CreatePostModal';
 import './Navigation.css';
 import AboutModal from '../AboutModal';
-
+import * as sessionActions from '../../store/session'
+import { useHistory } from 'react-router-dom';
 function Navigation({ isLoaded }){
+  const [demoCredential, setDemoCredential] = useState('demo@user.io')
+  const [demoPassword, setDemoPassword] = useState('password')
   const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+
+  const history = useHistory();
+  const handleDefaultButton = (e) => {
+    const credential = 'Demo-lition'
+    const password = 'password'
+
+   return dispatch(sessionActions.login({ credential, password }))
+        .then(() => history.push('/api/posts'))
+        .catch(
+            async (res) => {
+                const data = await res.json();
+                // if (data && data.errors) setErrors(data.errors);
+            }
+        );
+}
 
   let sessionLinks;
   if (sessionUser) {
@@ -18,8 +37,10 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        {/* <NavLink to="/login">Log In</NavLink> */}
         <LoginFormModal />
+        <button
+        onClick={handleDefaultButton}
+         className='demo-user-login-btn'>Log in as Demo User</button>
         <NavLink to="/signup">Sign Up</NavLink>
       </>
     );
@@ -28,10 +49,12 @@ function Navigation({ isLoaded }){
   return (
     <ul>
       <li className='nav-list'>
-        {/* <img src="../../../public/favicon.ico" alt="" /> */}
         <img className='product-hunt-logo' src="https://cdn-images-1.medium.com/fit/c/72/72/1*rrk0qqmHWx-jbaoC5MZlHA.png" alt="" />
-        <NavLink className="home-link" exact to="/">Home</NavLink>
+        <NavLink className="home-link" exact to="/">
+          <p>Home</p>
+        </NavLink>
         <AboutModal />
+
         {sessionUser ? <CreatePostModal /> : null}
         {isLoaded && sessionLinks}
       </li>
